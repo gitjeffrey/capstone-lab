6ab4438668ccc10b88690147b55f1bdd44ed3f31
jeff <jeff@puppetlabs.vm>
2017-07-13 10:23:39 -0700

    testtttt

---
+++ modules/host/manifests/init.pp
@@ -0,0 +1,8 @@
+class host {
+
+  host { 'testing.puppetlabs.vm':
+    ensure => present,
+    ip     => '127.0.0.1',
+  }
+
+}
--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,27 +1,41 @@
 class nginx {
 
-  package { 'nginx':
-    ensure => present, 
-    name => 'nginx',
+  case $facts['os']['family'] {
+    'redhat': {
+        $conf_root = '/etc/nginx'
+        $pkg_name = 'nginx'
+        $doc_root = '/var/www'
+    }
+    'windows': {
+      $conf_root = "C:\\Program Data\\nginx"
+      $pkg_name = 'nginx'
+      $doc_root = "${conf_root}\\html"
+    }
   }
 
-  file { '/var/www':
+  package { $pkg_name: 
+    ensure => present, 
+    name => $pkg_name,
+  }
+
+  file { $doc_root:
     ensure => directory,
   }
 
-  file { '/var/www/index.html':
+  file { "${doc_root}/index.html":
     ensure => present,
     source => 'puppet:///modules/nginx/index.html',
   }
 
-  file { '/etc/nginx/nginx.conf':
-    ensure => present,
+  file { "${conf_root}/nginx.conf" :
+    ensure  => present,
+    source => 'puppet:///modules/nginx/nginx.conf',
   }
 
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
+    subscribe => File["${conf_root}/nginx.conf"],
   }
 
 }
